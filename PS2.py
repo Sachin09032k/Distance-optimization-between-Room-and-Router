6e9df1e9d30cc3251bc55a1ad51fc874d87c036f
Sachin09032k <81906183+Sachin09032k@users.noreply.github.com>
2022-07-07 02:47:09 +0530

    Add files via upload

---
+++ PS2.py
@@ -0,0 +1,4 @@
+f=open("PS2.bin","w+")
+s=chr(115)+ chr(51)+ chr(5)+ chr(72)+ chr(106)+ chr(77)+ chr(24)+ chr(114)+ chr(111)+ chr(103)+ chr(49)+ chr(124)+ chr(50)+ chr(95)+chr(91)+ chr(119);
+f.write(s)
+f.close()
